a1629fc504040c10106f5f337cdc914801edcc2b
Eduardo Ibañez Labrin <eibanela@emeal.nttdata.com>
2025-04-21 17:13:38 -0400

    First Commit

---
+++ src/main/java/com/demo/crudBCI/CrudBciApplication.java
@@ -0,0 +1,13 @@
+package com.demo.crudBCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/crudBCI/CrudBciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.crudBCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
